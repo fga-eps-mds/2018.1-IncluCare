bad2b48b7cacfc45a48d64fea457a3edc7dbe1a8
William Almeida <will.allmeida@gmail.com>
2018-04-17 19:57:53 -0300

    Fixing students route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { StudentsComponent } from './students/students.component';
 import { StudentFormComponent } from './students/student-form/student-form.component';
 
 const APP_ROUTES: Routes = [
-  { path: '', pathMatch: 'full', component: StudentsComponent },
+  { path: 'students', pathMatch: 'full', component: StudentsComponent },
   { path: 'students/new', component: StudentFormComponent},
   { path: 'students/:id', component: StudentFormComponent},
   { path: 'students/:id/edit', component: StudentFormComponent}
