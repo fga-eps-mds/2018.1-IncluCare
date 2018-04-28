2760e6c9a18c07a363efac7717dc0be4fc323dca
IgorVeludo <velvet.guimaraes@gmail.com>
2018-04-28 18:21:18 -0300

    Updating RegisterFormComponent

--- src/app/login/register-form/register-form.component.spec.ts
+++ src/app/login/register-form/register-form.component.spec.ts
@@ -1,18 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { RegisterFormComponent } from './register-form.component';
+import { HttpModule }                       from '@angular/http';
+import { RegisterFormComponent }            from './register-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule }              from '@angular/router/testing';
+import { Angular2TokenService }             from 'angular2-token';
+import { AuthService }                      from "../../services/auth.service";
 
 describe('RegisterFormComponent', () => {
   let component: RegisterFormComponent;
   let fixture: ComponentFixture<RegisterFormComponent>;
 
   beforeEach(async( () => {
+    let tokenMock = jasmine.createSpyObj('tokenMock', ['validateToken', 'subscribe']);
+    tokenMock.validateToken.and.returnValue(tokenMock);
+
     TestBed.configureTestingModule({
-      declarations: [ RegisterFormComponent ]
+      declarations: [ RegisterFormComponent ],
+      imports: [
+        HttpModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        AuthService,
+        { provide: Angular2TokenService, useValue: tokenMock}
+      ]
     })
     .compileComponents();
   }));
-
   beforeEach(() => {
     fixture = TestBed.createComponent(RegisterFormComponent);
     component = fixture.componentInstance;
