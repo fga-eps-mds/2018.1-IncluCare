3e10777895ac5a19b2bcbab2a6eecb4e1f6a127c
IgorVeludo <velvet.guimaraes@gmail.com>
2018-04-29 10:30:18 -0300

    Update ProfileComponent
    Co-authored-by: byronkamal <byronkamal@hotmail.com>

--- src/app/profile/profile.component.spec.ts
+++ src/app/profile/profile.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Angular2TokenService }             from 'angular2-token';
 import { ProfileComponent } from './profile.component';
+import { HttpModule }                       from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule }              from '@angular/router/testing';
+import { AuthService }                      from "../services/auth.service";
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
 
   beforeEach(async(() => {
+    let tokenMock = jasmine.createSpyObj('tokenMock', ['validateToken', 'subscribe']);
+    tokenMock.validateToken.and.returnValue(tokenMock);
+
     TestBed.configureTestingModule({
-      declarations: [ ProfileComponent ]
+      declarations: [ ProfileComponent ],
+      imports: [
+        HttpModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule
+      ],
+      providers:[
+        AuthService,
+        {provide: Angular2TokenService, useValue: tokenMock}
+      ]
     })
     .compileComponents();
   }));
