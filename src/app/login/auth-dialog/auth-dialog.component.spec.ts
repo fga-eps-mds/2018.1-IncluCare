2815b0fde9e0f79ce5fa20ec547bc5871ce70e03
Mota <jpmota.unb@gmail.com>
2018-04-29 23:37:10 -0300

    Updating students,auth dialog, toolbar component

--- src/app/login/auth-dialog/auth-dialog.component.spec.ts
+++ src/app/login/auth-dialog/auth-dialog.component.spec.ts
@@ -1,14 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Angular2TokenService }             from 'angular2-token';
+import { HttpModule }                       from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule }              from '@angular/router/testing';
+import { AuthService }                      from "../../services/auth.service";
 import { AuthDialogComponent } from './auth-dialog.component';
+import { MaterializeAction } from "angular2-materialize";
 
 describe('AuthDialogComponent', () => {
   let component: AuthDialogComponent;
   let fixture: ComponentFixture<AuthDialogComponent>;
 
   beforeEach(async(() => {
+    let tokenMock = jasmine.createSpyObj('tokenMock', ['validateToken', 'subscribe']);
+    tokenMock.validateToken.and.returnValue(tokenMock);
     TestBed.configureTestingModule({
-      declarations: [ AuthDialogComponent ]
+      declarations: [ AuthDialogComponent ],
+      imports: [
+        HttpModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        MaterializeAction
+      ],
+      providers:[
+        AuthService,
+        {provide: Angular2TokenService, useValue: tokenMock}
+      ]
     })
     .compileComponents();
   }));
--- src/app/toolbar/toolbar.component.spec.ts
+++ src/app/toolbar/toolbar.component.spec.ts
@@ -1,5 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async,inject,ComponentFixture, TestBed } from '@angular/core/testing';
+import { Angular2TokenService }             from 'angular2-token';
+import { MaterializeAction } from "angular2-materialize";
+import { AuthService }                      from "../services/auth.service";
+import { HttpModule }                       from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule }              from '@angular/router/testing';
+import { AuthDialogComponent } from "../login/auth-dialog/auth-dialog.component";
 import { ToolbarComponent } from './toolbar.component';
 
 describe('ToolbarComponent', () => {
@@ -7,8 +13,21 @@ describe('ToolbarComponent', () => {
   let fixture: ComponentFixture<ToolbarComponent>;
 
   beforeEach(async(() => {
+    let tokenMock = jasmine.createSpyObj('tokenMock', ['validateToken', 'subscribe']);
+    tokenMock.validateToken.and.returnValue(tokenMock);
+
     TestBed.configureTestingModule({
-      declarations: [ ToolbarComponent ]
+      declarations: [ ToolbarComponent],
+      imports:[
+        HttpModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        MaterializeAction
+      ],
+      providers: [AuthService,
+        {provide: Angular2TokenService, useValue: tokenMock}
+      ]
     })
     .compileComponents();
   }));
