da5e2f2ec4285c0ef906e3d8e555e4fc4d63f766
Igor Aragão <roginaldosemog@gmail.com>
2018-05-01 11:57:52 -0300

    Fixing AppComponent tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,41 +1,53 @@
-import { TestBed, async } from '@angular/core/testing';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { HttpModule }                       from '@angular/http';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule }    from '@angular/router/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterTestingModule }              from '@angular/router/testing';
 import { Angular2TokenService }             from 'angular2-token';
-import { AppComponent } from './app.component';
+import { MaterializeModule }                from 'angular2-materialize';
+
+import { AppComponent }           from './app.component';
+
+import { AuthService }                      from "./services/auth.service";
+import { ToolbarComponent }       from './toolbar/toolbar.component';
+import { AuthDialogComponent }    from './login/auth-dialog/auth-dialog.component';
+import { LoginFormComponent }     from './login/login-form/login-form.component';
+import { RegisterFormComponent }  from './login/register-form/register-form.component';
 
 describe('AppComponent', ()=> {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
-    beforeEach(async(() => {
-      let tokenMock = jasmine.createSpyObj('tokenMock', ['validateToken', 'subscribe']);
+  beforeEach(async(() => {
+    let tokenMock = jasmine.createSpyObj('tokenMock', ['init', 'validateToken', 'subscribe']);
+    tokenMock.validateToken.and.returnValue(tokenMock);
 
     TestBed.configureTestingModule({
-      declarations: [ AppComponent ],
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [
+        AppComponent,
+        ToolbarComponent,
+        AuthDialogComponent,
+        LoginFormComponent,
+        RegisterFormComponent
+      ],
+      imports:[
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        MaterializeModule
+      ],
       providers: [
-        { provide: Angular2TokenService, useValue: tokenMock }
+        AuthService,
+        {provide: Angular2TokenService, useValue: tokenMock}
       ]
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ToolbarComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
