c863987e1381080091c4bcca4efdf2298148fe87
byronkamal <byronkamal@hotmail.com>
2018-05-08 00:47:35 -0300

    Creating route for the student panel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { LoginComponent }         from "./login/login.component";
 import { ProfileComponent }       from "./profile/profile.component";
 import { StudentsComponent }      from './students/students.component';
 import { StudentFormComponent }   from './students/student-form/student-form.component';
+import { StudentPanelComponent } from './students/student-panel/student-panel.component'
 
 const APP_ROUTES: Routes = [
   { path: '',                   component: LoginComponent,        pathMatch: 'full'                           },
@@ -15,7 +16,8 @@ const APP_ROUTES: Routes = [
   { path: 'students',           component: StudentsComponent,     pathMatch: 'full',  canActivate: [AuthGuard]},
   { path: 'students/new',       component: StudentFormComponent,                      canActivate: [AuthGuard]},
   { path: 'students/:id',       component: StudentFormComponent,                      canActivate: [AuthGuard]},
-  { path: 'students/:id/edit',  component: StudentFormComponent,                      canActivate: [AuthGuard]}
+  { path: 'students/:id/edit',  component: StudentFormComponent,                      canActivate: [AuthGuard]},
+  { path: 'panel',     component: StudentPanelComponent,                     }
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
