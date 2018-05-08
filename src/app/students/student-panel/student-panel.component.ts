374418c751f77d39a458473a0920cd651ed05269
byronkamal <byronkamal@hotmail.com>
2018-05-08 00:46:44 -0300

    Adding student panel

---
+++ src/app/students/student-panel/student-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-panel',
+  templateUrl: './student-panel.component.html',
+  styleUrls: ['./student-panel.component.css']
+})
+export class StudentPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
