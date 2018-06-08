b1e582538aabccb4024e27dca8d0c73456da519f
William Almeida <will.allmeida@gmail.com>
2018-06-07 21:48:09 -0300

    Adding DailyLogPanel test file
    Co-authored-by: gitgabiru <gabrielsoares.prog@gmail.com>

--- src/app/students/daily-log-panel/daily-log-panel.component.spec.ts
+++ src/app/students/daily-log-panel/daily-log-panel.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DailyLogService }                   from '../shared/dailylog.service';
+import { HttpModule }                       from '@angular/http';
+import { RouterTestingModule }              from '@angular/router/testing';
 import { DailyLogPanelComponent } from './daily-log-panel.component';
+import { FormsModule }                      from '@angular/forms';
 
 describe('DailyLogPanelComponent', () => {
   let component: DailyLogPanelComponent;
@@ -8,7 +11,13 @@ describe('DailyLogPanelComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DailyLogPanelComponent ]
+      imports: [
+        FormsModule,
+        HttpModule,
+        RouterTestingModule
+      ],
+      declarations: [ DailyLogPanelComponent ],
+      providers: [ DailyLogService ]
     })
     .compileComponents();
   }));
