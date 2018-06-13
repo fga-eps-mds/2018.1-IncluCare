2df7cb7d08170dce49f340d57e10c028e6f52b4f
IgorVeludo <velvet.guimaraes@gmail.com>
2018-06-13 13:18:48 -0300

    Testing addStudent method
    Co-authored-by: WillAllmeida <will.allmeida@gmail.com>

--- src/app/services/students.service.spec.ts
+++ src/app/services/students.service.spec.ts
@@ -3,6 +3,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { StudentsService } from './students.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpModule } from '@angular/http';
+import { Student } from '../shared/models'
 
 describe('StudentsService', () => {
   let service: StudentsService;
@@ -28,6 +29,27 @@ describe('StudentsService', () => {
     const userID = 1;
 
     const promise = service.getStudent(userID);
+    expect(promise).toBeDefined();
+  }));
+
+  it('addStudent should return a promise', inject([StudentsService], (service: StudentsService) => {
+    const user: Student = {
+      id: 445,
+      name: 'adalberto',
+      birth_date: '12-12-1929',
+      age: 18,
+      year: 9,
+      student_class: 'a',
+      shift: 'matutino',
+      nationality: 'brasileiro',
+      father_name: 'jair',
+      mother_name: 'edineia',
+      responsible: 'responsavel',
+      address: 'riacho fundo',
+      parent_telephone: 444122323,
+
+    };
+    const promise = service.addStudent(user);
     expect(promise).toBeDefined();
   }));
 
