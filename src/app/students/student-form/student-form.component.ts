f565ce48f07b9bf013867a9332d3fbfc016802c3
Igor Aragão <roginaldosemog@gmail.com>
2018-06-13 23:52:17 -0300

    Refactoring StudentForm

--- src/app/students/student-form/student-form.component.ts
+++ src/app/students/student-form/student-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
-import { Student } from "../../shared/models";
+import { MaterializeDirective } from 'angular2-materialize';
+
+import { Student } from '../../shared/models';
 import { StudentsService } from '../../services/students.service';
 
 @Component({
@@ -13,6 +15,9 @@ export class StudentFormComponent implements OnInit {
 
   name: string;
   student: Student = new Student();
+
+  classes = [];
+  selectedClass: number;
 
   constructor(
 
@@ -27,17 +32,56 @@ export class StudentFormComponent implements OnInit {
 
       this.name = id ? 'Editar Estudante' : 'Criar Estudante';
 
-      if (!id)
-        return;
+      if(!id)
+      return;
 
       this.studentService.getStudent(id)
-        .subscribe(
-          student => this.student = student,
-          response => {});
+      .subscribe(
+        student => this.student = student,
+        response => {}
+      );
     });
+
+    this.initClasses();
+  }
+
+  initClasses(){
+    this.classes = [
+      {value:1, name:"1º A"},
+      {value:2, name:"1º B"},
+      {value:3, name:"2º A"},
+      {value:4, name:"2º B"}
+    ]
+  }
+
+  selectClass(value: any){
+
+    switch(value) {
+      case '1':
+      this.student.year = 1;
+      this.student.student_class = 'A';
+      break;
+      case '2':
+      this.student.year = 1;
+      this.student.student_class = 'B';
+      break;
+      case '3':
+      this.student.year = 2;
+      this.student.student_class = 'A';
+      break;
+      case '4':
+      this.student.year = 2;
+      this.student.student_class = 'B';
+      break;
+      default:
+      console.log("Invalid choice");
+      break;
+    }
   }
 
   save() {
+    this.selectClass(this.selectedClass);
+
     var result;
 
     if (this.student.id){
